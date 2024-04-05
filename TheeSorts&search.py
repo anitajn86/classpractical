de064fdba2273d10cf8b70a1186d4a565bd276e0
Anita <anitajn7990@gmail.com>
2024-04-05 17:30:57 +0300

    daily commit

--- TheeSorts&search.py
+++ TheeSorts&search.py
@@ -10,3 +10,15 @@ nums=[4,6,7,9,10,24,13,2,3,4,]
 bubble_sort(nums)
 print("Sorted array",nums)
         
+
+def buble_sort(digits):
+    n=len(digits)
+    for i in range(n):
+        for j in range(n-i-1):   #even(n-2)worked. this is because after the two loops, 
+                              #the last 2 i elements are already in place. 
+            if digits[j]>digits[j+1]:
+                digits[j],digits[j+1]=digits[j+1],digits[j]
+
+digits=[5,3,8,1,2]
+buble_sort(digits)
+print("This is the sorted array",digits)
